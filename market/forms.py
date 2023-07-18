e8ac56ac7d685318b9068c680e7ae59e17b094eb
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2023-07-18 22:47:31 +0300

    validated user existence using Flashes and Flask Validations

--- market/forms.py
+++ market/forms.py
@@ -1,13 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField , SubmitField, TextAreaField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterFrom(FlaskForm):
+    
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exist! Please try a different username')
+        
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exist! Please try a different email address')
+        
+        
+    
     username = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:', validators=Length(min=6))
+    password1 = PasswordField(label='Password:', validators=[Length(min=6)])
     password2 = PasswordField(label='Confirm Password:', validators=[DataRequired(), EqualTo('password1')])
     submit = SubmitField(label='Create Account')
-    
     
     
